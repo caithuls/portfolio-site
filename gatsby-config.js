8b5fa22237f7edd3f1c3212217022fbf8242704a
Caitlin Hulsey <caithuls8@gmail.com>
2019-11-13 10:27:11 -0500

    added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,48 @@
 module.exports = {
   siteMetadata: {
-    title: `Caitlin Hulsey`,
-    description: `Caitlin Hulsey`,
-    author: `@caithuls`
+    title: "Caitlin Hulsey",
+    description: "Caitlin Hulsey",
+    author: "@caithuls"
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `Caitlin Hulsey`,
-        short_name: `caithuls`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#4dc0b5`,
-        display: `minimal-ui`,
-        icon: `src/images/folds.png`
+        name: "Caitlin Hulsey",
+        short_name: "caithuls",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#4dc0b5",
+        display: "minimal-ui",
+        icon: "src/images/folds.png"
       }
     },
-    `gatsby-plugin-postcss`,
+    "gatsby-plugin-postcss",
     {
-      resolve: `gatsby-plugin-purgecss`,
+      resolve: "gatsby-plugin-purgecss",
       options: {
         tailwind: true,
-        purgeOnly: [`src/css/style.css`]
+        purgeOnly: ["src/css/style.css"]
       }
     },
-    `gatsby-plugin-offline`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./src/data/",
+        name: "data"
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./src/images/",
+        name: "images"
+      },
+    },
+    "gatsby-transformer-json",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-offline"
   ]
 };
