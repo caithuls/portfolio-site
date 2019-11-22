fb416448db779bc8d77b31939dee2decb240f70b
Caitlin Hulsey <caithuls8@gmail.com>
2019-11-22 12:28:28 -0500

    added etsy link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaEtsy } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { MdEmail } from "react-icons/md";
@@ -8,17 +9,22 @@ function Footer() {
         <footer className="bg-indigo-700">
             <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-white text-xl">
                 <div class="flex content-around flex-wrap">
-                    <div class="w-1/3 p-2">
+                    <div class="w-1/4 p-2">
                         <a href="mailto:caithuls8@gmail.com">
                             <MdEmail />
                         </a>
                     </div>
-                    <div class="w-1/3 p-2">
+                    <div class="w-1/4 p-2">
+                        <a href="https://www.etsy.com/shop/caithuls">
+                            <FaEtsy />
+                        </a>
+                    </div>
+                    <div class="w-1/4 p-2">
                         <a href="https://twitter.com/caithuls">
                             <FaTwitter />
                         </a>
                     </div>
-                    <div class="w-1/3 p-2">
+                    <div class="w-1/4 p-2">
                         <a href="https://instagram.com/caithuls">
                             <FaInstagram />
                         </a>
